fa9193bd163fbffe1413c167881aafe90bea41ee
shineexxx <Arseny.livegames@gmail.com>
2023-10-25 21:36:27 +0300

    тест
    написал интересный код

---
+++ Assets/bbb.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bbb : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("xuy");
+    }
+}
